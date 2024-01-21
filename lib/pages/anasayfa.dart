5ba95f586f9358432f021dba1b938b81420c7696
anielyavuz <52561909+anielyavuz@users.noreply.github.com>
2024-01-21 11:48:43 +0300

    11:48 21.01.2024

--- lib/pages/anasayfa.dart
+++ lib/pages/anasayfa.dart
@@ -1,16 +1,16 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:heybaby/pages/storyImages.dart';
 
 class AnaSayfa extends StatelessWidget {
   final List<String> storyImages;
   final Map<String, dynamic>? userData;
+
   const AnaSayfa({Key? key, this.userData, required this.storyImages})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
         // Story Circles
         Container(
@@ -41,84 +41,71 @@ class AnaSayfa extends StatelessWidget {
           ),
         ),
         // Resim
-        Expanded(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                // Resim
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.75,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Image.network(
-                    'https://firebasestorage.googleapis.com/v0/b/heybaby-d341f.appspot.com/o/Leonardo_Diffusion_XL_A_baby_cartoon_in_the_womb_make_its_age_2.jpg?alt=media&token=f1a7f0dc-b9b5-46e7-891f-ca4a76c78712',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                // Alıntılar Container'ı
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.75,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 10.0),
-                      Text(
-                        'Bebeğinizden Mesaj',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 5.0),
-                      AutoSizeText(
-                        """Merhaba Annecim!
+        Container(
+          width: MediaQuery.of(context).size.width * 0.95,
+          height: MediaQuery.of(context).size.height * 0.25,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/heybaby-d341f.appspot.com/o/Leonardo_Diffusion_XL_A_baby_cartoon_in_the_womb_make_its_age_2.jpg?alt=media&token=f1a7f0dc-b9b5-46e7-891f-ca4a76c78712',
+            fit: BoxFit.cover,
+          ),
+        ),
+        // Alıntılar Container'ı
+        Container(
+          width: MediaQuery.of(context).size.width * 0.95,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Bebeğinizden Mesaj',
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5.0),
+              Text(
+                """Merhaba Annecim!
 4 aylık oldum ve seninle daha fazla bağ kuruyorum. Karnında sıcacık ve güvende olmak harika!""",
-                        style: TextStyle(fontSize: 20.0),
-                        maxLines: 3, // Maksimum satır sayısı
-                        overflow:
-                            TextOverflow.ellipsis, // Fazla metni gösterme şekli
-                      ),
-                    ],
-                  ),
+                style: TextStyle(fontSize: 15.0),
+                maxLines: 4,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ),
+        // Aktiviteler Container'ı
+        Container(
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width * 0.95,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 5.0),
+                Text(
+                  'Yaklaşan Aktiviteler',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                 ),
-                // Aktiviteler Container'ı
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.25,
-                  width: MediaQuery.of(context).size.width * 0.75,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        SizedBox(height: 5.0),
-                        Text(
-                          'Yakındaki Aktiviteler',
-                          style: TextStyle(
-                              fontSize: 20.0, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 3.0),
-                        // Yakındaki aktivitelerin listesi
-                        ListTile(
-                          title: Text('Yürüyüş'),
-                          subtitle: Text('23 Ocak 2024, 15:00'),
-                        ),
-                        ListTile(
-                          title: Text('Kahve Buluşması'),
-                          subtitle: Text('25 Ocak 2024, 18:30'),
-                        ),
-                        ListTile(
-                          title: Text('Kitap Okuma'),
-                          subtitle: Text('27 Ocak 2024, 20:00'),
-                        ),
-                      ],
-                    ),
-                  ),
+                ListTile(
+                  title: Text('Yürüyüş'),
+                  subtitle: Text('23 Ocak 2024, 15:00'),
+                ),
+                ListTile(
+                  title: Text('Kahve Buluşması'),
+                  subtitle: Text('25 Ocak 2024, 18:30'),
+                ),
+                ListTile(
+                  title: Text('Kitap Okuma'),
+                  subtitle: Text('27 Ocak 2024, 20:00'),
                 ),
               ],
             ),
@@ -127,4 +114,36 @@ class AnaSayfa extends StatelessWidget {
       ],
     );
   }
+}
+
+class StoryScreen extends StatelessWidget {
+  final List<String> storyImages;
+
+  const StoryScreen({Key? key, required this.storyImages}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // StoryScreen widget'ı için gerekli ekran tasarımı buraya eklenebilir
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Story Screen'),
+      ),
+      body: ListView.builder(
+        itemCount: storyImages.length,
+        itemBuilder: (context, index) {
+          return Image.network(storyImages[index]);
+        },
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: AnaSayfa(
+      storyImages: [
+        // Story Images'larınızı ekleyin
+      ],
+    ),
+  ));
 }
