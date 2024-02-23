4fa450a7d564f77bd4d48d1b50e70921bb0ba343
mutlucanunal <mutlucan.unal@gmail.com>
2024-02-24 01:11:48 +0300

    01:11 24.02.2024

--- lib/pages/subpages/spinningWheel.dart
+++ lib/pages/subpages/spinningWheel.dart
@@ -36,10 +36,10 @@ class _RotatingHalfWheelState extends State<RotatingHalfWheel> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: SafeArea(
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
             child: Center(
               child: Text(
                   "Su Hedefi " +
@@ -48,104 +48,104 @@ class _RotatingHalfWheelState extends State<RotatingHalfWheel> {
                       _targetValue.toString(),
                   style: TextStyle(fontSize: 22, color: Colors.black)),
             ),
+            flex: 1,
           ),
-          flex: 1,
-        ),
-        Expanded(
-          flex: 4,
-          child: Stack(
-            children: [
-              Center(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width-100,
-                  height: MediaQuery.of(context).size.width-100,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 30,
-                    value: _historyValue / _targetValue,
-                    backgroundColor:
-                        Color.fromRGBO(0, 203, 255, 1).withOpacity(0.2),
-                    color: Color.fromRGBO(0, 203, 255, 1),
+          Expanded(
+            flex: 4,
+            child: Stack(
+              children: [
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width-100,
+                    height: MediaQuery.of(context).size.width-100,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 30,
+                      value: _historyValue / _targetValue,
+                      backgroundColor:
+                          Color.fromRGBO(0, 203, 255, 1).withOpacity(0.2),
+                      color: Color.fromRGBO(0, 203, 255, 1),
+                    ),
                   ),
                 ),
-              ),
-              Center(
-                child: Container(
-                  width: 250,
-                  height: 250,
-                  child: ListWheelScrollView(
-                    itemExtent: 100,
-                    onSelectedItemChanged: (int index) {
-                      setState(() {
-                        _selectedItem = _items.keys.elementAt(index);
-                        _selectedValue = _items[_items.keys.elementAt(index)]!;
-                      });
-                    },
-                    children: _items.keys
-                        .map(
-                          (e) => Container(
-                            decoration: BoxDecoration(
-                              color: e == _selectedItem ? Color.fromARGB(100, 50, 173, 54) : Color.fromARGB(112, 234, 34, 20),
-                              borderRadius: BorderRadius.all(Radius.circular(50))
-                            ),
-                            width: e == _selectedItem ? 100 : 60,
-                            height: e == _selectedItem ? 100 : 60,
-                            child: Center(
-                              child: Image.asset(
-                                'assets/$e.png', // Örneğin, bardak.png, şişe.png gibi
-                                width: 200,
-                                height: 200,
+                Center(
+                  child: Container(
+                    width: 250,
+                    height: 250,
+                    child: ListWheelScrollView(
+                      itemExtent: 100,
+                      onSelectedItemChanged: (int index) {
+                        setState(() {
+                          _selectedItem = _items.keys.elementAt(index);
+                          _selectedValue = _items[_items.keys.elementAt(index)]!;
+                        });
+                      },
+                      children: _items.keys
+                          .map(
+                            (e) => Container(
+                              decoration: BoxDecoration(
+                                color: e == _selectedItem ? Color.fromARGB(100, 50, 173, 54) : Color.fromARGB(112, 234, 34, 20),
+                                borderRadius: BorderRadius.all(Radius.circular(50))
+                              ),
+                              width: e == _selectedItem ? 100 : 60,
+                              height: e == _selectedItem ? 100 : 60,
+                              child: Center(
+                                child: Image.asset(
+                                  'assets/$e.png', // Örneğin, bardak.png, şişe.png gibi
+                                  width: 200,
+                                  height: 200,
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                        .toList(),
+                          )
+                          .toList(),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        Expanded(
-          flex: 5,
-          child: Column(
-            children: [
-              Text(
-                _selectedItem + " (" + _selectedValue + "ml)",
-                style: TextStyle(fontSize: 22, color: Colors.black),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _history.insert(0, _selectedItem);
-                    if (_historyValue + int.parse(_selectedValue) <=
-                        _targetValue) {
-                      _historyValue += int.parse(_selectedValue);
-                    } else {
-                      _historyValue = _targetValue;
-                    }
-                  });
-                },
-                child: Text('Kaydet'),
-              ),
-              SizedBox(height: 20),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: _history
-                        .map(
-                          (item) => ListTile(
-                            title: Text(item),
-                          ),
-                        )
-                        .toList(),
+          Expanded(
+            flex: 5,
+            child: Column(
+              children: [
+                Text(
+                  _selectedItem + " (" + _selectedValue + "ml)",
+                  style: TextStyle(fontSize: 22, color: Colors.black),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _history.insert(0, _selectedItem);
+                      if (_historyValue + int.parse(_selectedValue) <=
+                          _targetValue) {
+                        _historyValue += int.parse(_selectedValue);
+                      } else {
+                        _historyValue = _targetValue;
+                      }
+                    });
+                  },
+                  child: Text('Kaydet'),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: _history
+                          .map(
+                            (item) => ListTile(
+                              title: Text(item),
+                            ),
+                          )
+                          .toList(),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        )
-      ],
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
